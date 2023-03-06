e6c6110c3d0af3e0b4de2e81737c1258e457e3e2
Martin Creedy <martin@openskydevelopment.co.uk>
2023-03-06 23:11:36 +0700

    Foundry fork test

--- apps/contracts/test/Example.t.sol
+++ apps/contracts/test/Example.t.sol
@@ -32,14 +32,10 @@ contract FooTest is StdCheats, Test {
     /// @dev Test that runs against a fork of Ethereum Mainnet. You need to set `API_KEY_ALCHEMY` in your environment
     /// for this test to run - you can get an API key for free at https://alchemy.com.
     function testFork_Example() external {
-        string memory alchemyApiKey = vm.envOr("API_KEY_ALCHEMY", string(""));
-        // Silently pass this test if the user didn't define the API key.
-        if (bytes(alchemyApiKey).length == 0) {
-            return;
-        }
+        string memory mainnet = vm.envOr("RPC_URL_MAINNET", string(""));
 
         // Run the test normally, otherwise.
-        vm.createSelectFork({urlOrAlias: "ethereum", blockNumber: 16_428_000});
+        vm.createSelectFork({urlOrAlias: mainnet, blockNumber: 16_428_000});
         address usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
         address holder = 0x7713974908Be4BEd47172370115e8b1219F4A5f0;
         uint256 actualBalance = IERC20(usdc).balanceOf(holder);
