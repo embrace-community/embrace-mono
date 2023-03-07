474d2f463976c5858e900aea7a5d1f9deb352658
Martin Creedy <martin@openskydevelopment.co.uk>
2023-03-07 21:55:13 +0700

    Remove dist

--- packages/ui/dist/atoms/Button.mjs
+++
@@ -1,1 +0,0 @@
-export { a as Button } from '../chunk-HRCD2FXU.mjs';
--- packages/ui/dist/chunk-HRCD2FXU.mjs
+++
@@ -1,5 +0,0 @@
-import { jsxs } from 'react/jsx-runtime';
-
-var t=({primary:o=!1,label:e="Default",size:r="small"})=>jsxs("button",{className:"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded",children:[e,"zz"]});
-
-export { t as a };
