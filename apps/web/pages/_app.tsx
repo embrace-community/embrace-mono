b207c546877895599ab4124d5a9edd54b0d441be
Martin Creedy <martin@openskydevelopment.co.uk>
2023-03-07 17:07:16 +0700

    Nextjs app

---
+++ apps/web/pages/_app.tsx
@@ -0,0 +1,11 @@
+import { AppProps } from "next/app";
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <h1>Layout</h1>
+      <Component {...pageProps} />
+    </>
+  );
+}
