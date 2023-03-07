9fce624206398cf9caa40ba1a6ac902018d2aec6
Martin Creedy <martin@openskydevelopment.co.uk>
2023-03-07 21:56:39 +0700

    Ignore dist

--- packages/ui/atoms/Button.tsx
+++ packages/ui/atoms/Button.tsx
@@ -18,7 +18,7 @@ export const Button = ({
       // }}
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
     >
-      {label}zz
+      {label}
     </button>
   );
 };
