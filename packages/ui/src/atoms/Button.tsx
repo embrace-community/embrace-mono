660182f9b44cca378a52dc5206e59f17b2d932a4
Martin Creedy <martin@openskydevelopment.co.uk>
2023-03-28 11:52:30 +0100

    Button changes

--- packages/ui/src/atoms/Button.tsx
+++ packages/ui/src/atoms/Button.tsx
@@ -1,15 +1,17 @@
 import classNames from "classnames";
 
 type Size = "small" | "medium" | "large";
+type Variant = "primary" | "secondary" | "default" | "disabled";
+
 interface Props {
-	primary?: boolean;
+	variant?: Variant;
 	size?: Size;
 	label?: string;
 	onClick?: () => void;
 }
 
 export const Button = ({
-	primary = false,
+	variant = "default",
 	label = "Button",
 	size = "medium",
 	onClick = () => {}
@@ -20,7 +22,7 @@ export const Button = ({
 				"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded":
 					true,
 				"bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded":
-					primary,
+					variant == "primary",
 				"text-sm": size === "small",
 				"text-md": size === "medium",
 				"text-lg": size === "large"
