73b4ed2ce74cd50457c633897961693ce7acfaaa
Martin Creedy <martin@openskydevelopment.co.uk>
2024-09-23 14:49:56 +0100

    Document description

--- apps/foundation-landing/pages/_document.tsx
+++ apps/foundation-landing/pages/_document.tsx
@@ -5,10 +5,19 @@ export default function Document() {
 		<Html lang="en">
 			<Head>
 				<link rel="icon" href="/images/embrace-foundation.png" />
-				<link rel="apple-touch-icon" href="/images/embrace-foundation.png" />
-				<meta property="og:title" content="Embrace: Creator" />
-				<meta property="og:description" content="Embrace" />
-				<meta property="og:image" content="/images/embrace-foundation.png" />
+				<link
+					rel="apple-touch-icon"
+					href="/images/embrace-foundation.png"
+				/>
+				<meta property="og:title" content="Embrace Community" />
+				<meta
+					property="og:description"
+					content="Enabling communities to take back the reigns and steer their own destiny."
+				/>
+				<meta
+					property="og:image"
+					content="/images/embrace-foundation.png"
+				/>
 				<meta property="og:image:width" content="120" />
 				<meta property="og:image:height" content="120" />
 			</Head>
